f702b7db12d3453116e1a933f149198339e807ac
= <=>
2018-11-03 16:29:55 +0200

    Created db migration for tours

---
+++ database/migrations/2018_11_03_121924_create_tours_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateToursTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tours', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->string('description');
+            $table->string('location');
+            $table->unsignedInteger('cat_id');
+            $table->decimal('price')->default(0);
+            $table->string('photo')->nullable();
+            $table->foreign('cat_id')->references('id')->on('tour_categories')->onDelete('cascade')->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tours');
+    }
+}
